ee25bc86e3876ffd1b2b8563505bb3de5d835b84
Simon Babin-Tremblay <simonbabint@live.ca>
2016-08-05 14:43:35 -0400

    convert get todo id with sequelize

--- server.js
+++ server.js
@@ -37,13 +37,17 @@ app.get('/todos', function (req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
 	
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	db.todo.findById(todoId).then(function (todo) {
+		if (!!todo){
+			res.json(todo.toJSON());
+		} else {
+			res.status(400).send();
+		}
+	}, function (e) {
+		res.status(500).send();
+	});
+
 	
 });
 
@@ -56,26 +60,6 @@ app.post('/todos', function (req, res) {
 	}, function (e) {
 		res.status(400).json(e);
 	});
-	//call create on db.todo
-	// 	respond with 200 and todo
-	//	res.status(400).json(e)
-	
-	
-	
-	/*if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();		
-	}
-	
-	//set body.description to be trimmed value
-	body.description = body.description.trim();	
-	
-	//add id field
-	body.id = todoNextId++;
-	
-	//push body into array
-	todos.push(body);
-	
-	res.json(body);*/
 });
 
 //DELETE /todos/:id
